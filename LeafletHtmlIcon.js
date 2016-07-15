0aab7412396842e9204e41356c4b740c05bdc03c
Simone Albano <dc.jest@gmail.com>
2016-07-16 00:38:07 +0200

    Update LeafletHtmlIcon.js
    Fixed a problem caused by div alignment before leaflet's translation: only first block was OK, because it floated to 0,0 by default. 
    Also implemented a few options like iconSize and iconAnchor (was listed in available options but it didn't work for me) and an options to add or overwrite some custom CSS to the div.

--- LeafletHtmlIcon.js
+++ LeafletHtmlIcon.js
@@ -1,31 +1,49 @@
-/**
- * Plugin for adding arbitrary HTML markers to a Leaflet map
- * https://github.com/dwnoble/LeafletHtmlIcon
- * 
- * Public domain
- * 
- */
+      /**
+       * Plugin for adding arbitrary HTML markers to a Leaflet map
+       * https://github.com/dwnoble/LeafletHtmlIcon
+       * 
+       * Public domain
+       * 
+       */
 
-L.HtmlIcon = L.Icon.extend({
-	options: {
-		/*
-		html: (String) (required)
-		iconAnchor: (Point)
-		popupAnchor: (Point)
-		*/
-	},
+      L.HtmlIcon = L.Icon.extend({
+        options: {
+          /*
+          html: (String) (required)
+          css: (Object)
+          iconAnchor: (Point)
+          popupAnchor: (Point)
+          */
+        },
 
-	initialize: function (options) {
-		L.Util.setOptions(this, options);
-	},
+        initialize: function (options) {
+          L.Util.setOptions(this, options);
+        },
 
-	createIcon: function () {
-		var div = document.createElement('div');
-		div.innerHTML = this.options.html;
-		return div;
-	},
+        createIcon: function () {
+          var div = document.createElement('div');
+          div.innerHTML = this.options.html;
+          div.style.overflow = "hidden";
+          div.style.position = "absolute";
+          div.style.top = "0px";
+          div.style.left = "0px";
+          div.style.margin = "0px";
+          div.style.padding = "0px";
+          for(var i in this.options.css) {
+          	div.style[i] = this.options.css[i];
+          }
+          if (this.options.iconSize) {
+            div.style.width = this.options.iconSize[0]+"px";
+            div.style.height = this.options.iconSize[1]+"px";
+          }
+          if(this.options.iconAnchor) {
+            div.style.marginTop = -this.options.iconAnchor[0]+"px";
+            div.style.marginLeft = -this.options.iconAnchor[1]+"px";
+          }
+          return div;
+        },
 
-	createShadow: function () {
-		return null;
-	}
-});
+        createShadow: function () {
+          return null;
+        }
+      });
